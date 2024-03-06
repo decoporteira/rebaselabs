9aa296c2147f31ed050bfe9e1baf759b1dfa1f26
André Pereira <andreporteira@gmail.com>
2024-03-06 17:53:53 -0300

    WIP: erro nos testes

--- public/main.js
+++ public/main.js
@@ -1,13 +1,42 @@
 const fragment = new DocumentFragment();
 const url = '/tests';
+const itemsPerPage = 10
+let currentPage = 1;
 
 fetch(url).
   then((response) => response.json()).
   then((data) => {
     data.forEach(function(patient) {
+      patient.data_nascimento_paciente = new Date(patient.data_nascimento_paciente);
+      patient.data_exame = new Date(patient.data_exame);
       const li = document.createElement('li');
-      li.textContent = `Id: ${patient.id} | Nome: ${patient.nome_paciente}`;
-      li.classList.add('item-patient');
+      li.innerHTML = `<h2>Cód. Exame: ${patient.id}</h2>
+
+      
+      <h3>Paciente</h3>
+      <div class='data-patient'> 
+        <strong>Nome:</strong> ${patient.nome_paciente} | 
+        <strong>Nascimento:</strong> ${patient.data_nascimento_paciente.toLocaleDateString('pt-BR')} | 
+        <strong>Endereço:</strong> ${patient.endereco_paciente} | 
+        <strong>Cidade:</strong> ${patient.cidade_paciente} | 
+        <strong>Estado:</strong> ${patient.estado_paciente} 
+      </div>
+      <h3>Médico</h3>
+      <div class='data-doctor'> 
+        <strong>Nome:</strong> ${patient.nome_medico} |
+        <strong>CRM:</strong> ${patient.crm_medico} |
+        <strong>CRM Estado:</strong> ${patient.crm_medico_estado} |
+        <strong>Email:</strong> ${patient.email_medico}
+      </div>
+      <h3>Exame</h3>
+      <div class='data-exam'> 
+        <strong>Token do exame:</strong> ${patient.token_resultado_exame}
+        <strong>Data do exame:</strong> ${patient.data_exame.toLocaleDateString('pt-BR')} |
+        <strong>Tipo do exame:</strong> ${patient.tipo_exame} |
+        <strong>Limites:</strong> ${patient.limites_tipo_exame} |
+        <strong>Resultado:</strong> ${patient.resultado_tipo_exame}
+      </div>`
+      li.classList.add('item-exam');
       fragment.appendChild(li);
     })
   }).
@@ -17,4 +46,3 @@ fetch(url).
   catch(function(error) {
     console.log(error);
   });
-
--- spec/tests/test_spec.rb
+++ spec/tests/test_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../../server'
 require 'rack/test'
 
+require 'spec_helper'
+
 RSpec.describe 'CVS Import App' do
   include Rack::Test::Methods
 
@@ -15,8 +17,11 @@ RSpec.describe 'CVS Import App' do
     expect(last_response.content_type).to eq('application/json')
   end
 
-  it 'Vê os resultados na página' do
+  it 'Vê os resultados na página', type: :feature do
     get '/home'
+    puts last_response.body
+    expect(last_response).to be_ok
+    expect(last_response.content_type).to eq('text/html;charset=utf-8')
     expect(last_response.body).to include('Emilly Batista Neto')
   end
 end
